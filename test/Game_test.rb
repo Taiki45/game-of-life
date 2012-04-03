f2c0cc6a7ebca79779a4416f761a2d87b2976a1f
Taiki ONO <taiks.4559@gmail.com>
2012-04-03 16:09:39 +0900

    Fix class name

--- test/Game_test.rb
+++ test/Game_test.rb
@@ -6,7 +6,7 @@ MiniTest::Unit.autorun
 DEAD = 0
 ALIVE = 1
 
-class TestLife < MiniTest::Unit::TestCase
+class TestGame < MiniTest::Unit::TestCase
   def test_game
     code = 'def print_state; end'
     Game.class_eval code
